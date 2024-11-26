808f621c8836de5221d0a889e61945dfb5c26f25
Eranda Jayasinghe <erandajayasinghe724@gmail.com>
2024-11-26 05:35:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,23 @@
-from flask import Flask, render_template, url_for, request, jsonify
-
+from flask import Flask, render_template, request
 from dep import Speller
 
 app = Flask(__name__)
-
 speller = Speller(threshold=3)
 
 @app.route('/')
-def home ():
+def home():
     return render_template('index.html')
 
 @app.route('/get_correction', methods=['POST'])
-def run_function():
-    text = request.form.get('text')
-    if text:
-        text = text.lower()
+def get_correction():
+    text = request.form.get('text', '').strip()
+    if not text:
+        return render_template('index.html', corrected_text="Please enter a valid word.")
+
+    text = text.lower()
     data = speller.get_candidates(text)
 
-    corrected_text = data if data else [('word is correct!',0)]
-    print(corrected_text)
-    
+    corrected_text = data if data else [('The word is correct!', 0)]
     return render_template('index.html', corrected_text=corrected_text)
 
 if __name__ == "__main__":
